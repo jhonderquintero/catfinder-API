aa7c8a644358b2cb6c92511d5b6501352b30b5db
jhonderquintero <Jhonderquintero@gmail.com>
2021-03-05 21:44:08 -0400

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,18 +1,3 @@
-/* 
-    User Routes / Authorizations
-    Host + /api/auth
-
-    CAT API KEY:
-    91960c26-0727-471d-92ed-ba210e268b4f
-
-    MONGO ATLAS:
-    DB_USER: catfinder_user
-    PASS: P3wTUjkqPafO7qFJ
-
-    OAuth GOOGLE:
-    Client ID = 890895167918-o17h2hmpatre78rjjd11o0rocf67pk53.apps.googleusercontent.com
-    Client Secret = U17z_55tbQwdNOPHohhBFRm0
-*/
 import {userLogin, newUser, userRegistration, userLogout} from '../controllers/auth';
 import { validateErrors } from '../middlewares/validateErrors';
 import {check} from 'express-validator';
